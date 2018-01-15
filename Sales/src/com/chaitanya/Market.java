9e2e99391c389a29033d835df466fc243faafaa3
cvgokhale <cvgokhale@gmail.com>
2018-01-15 11:46:49 -0500

    This is first commit

---
+++ Sales/src/com/chaitanya/Market.java
@@ -0,0 +1,16 @@
+package com.chaitanya;
+
+public class Market {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SalesData s1 = new SalesData();
+		marketWelcome();
+		s1.call();
+	}
+	
+	private static void marketWelcome(){
+		System.out.println("Welcome");
+	}
+
+}
---
+++ Sales/src/com/chaitanya/SalesData.java
@@ -0,0 +1,9 @@
+package com.chaitanya;
+
+public class SalesData {
+
+	int data[]={1,2,4};
+	public void call(){
+		System.out.println("Display Data"+data);
+	}
+}
